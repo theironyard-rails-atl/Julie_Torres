6bc36ca68d028c127ce5527b6f23e33f2b6b4bd8
Julie Torres <reeddetorres@gmail.com>
2014-07-31 16:17:14 -0400

    Got ask_question working

--- HW 7.31/survey.rb
+++ HW 7.31/survey.rb
@@ -1,11 +1,12 @@
-require 'pry'
+Questions = ["How many toes do you have on each foot?", "How many people named Oprah do you know?",
+  "How many baby-daddies do you have?", "How many times have you been on Facebook today?", "What is your favorite number?"
+]
 
 class Survey
-  attr_reader :question_count #, :next_question
-  def initialize
+  attr_reader :question_count, :questions
+  def initialize(questions, question_count)
     @question_count = 5
-    @questions = ["q1", "q2", "q3", "q4", "q5"]
-    #@next_question = @questions[0]
+    @questions = questions
 
   end
 
@@ -15,16 +16,17 @@ class Survey
 
   def ask_question
       @question_count -= 1
-      puts @questions.shift(0)
-      #@next_question.shift
+      puts @questions[0]
+      @questions.delete_at(0)
+      @answer = gets.chomp.to_i
   end
 end
 
-# survey = Survey.new
-# puts survey.question_count
-#
-# until survey.finished?
-#   survey.asks_question
-# end
-#
-# puts "Survey finished"
+survey = Survey.new(Questions, 5)
+puts survey.question_count
+
+until survey.finished?
+  survey.ask_question
+end
+
+puts "Survey finished"
