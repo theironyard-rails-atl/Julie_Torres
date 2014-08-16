90312b911bef70585a324522f23df1e85881147f
Julie Torres <reeddetorres@gmail.com>
2014-08-16 13:41:08 -0400

    Add About Us page

--- twitter_app/spec/requests/static_pages_spec.rb
+++ twitter_app/spec/requests/static_pages_spec.rb
@@ -14,5 +14,13 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-  end  
+  end
+
+   describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
