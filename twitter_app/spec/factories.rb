3f2cf4c11b5a0f7727edb5be855b36d726accc8f
Julie Torres <reeddetorres@gmail.com>
2014-08-18 14:49:35 -0400

    Add Factory Girl gem & user page tests

---
+++ twitter_app/spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Julie Torres"
+    email    "reeddetorres@gmail.com"
+    password "foobar12"
+    password_confirmation "foobar12"
+  end
+end
